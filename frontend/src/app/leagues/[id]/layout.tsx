bc6c36937cc17cc8bdb4b66b60a10ea0cc65c41e
chengyitang <tangcy1002@gmail.com>
2025-05-25 23:19:56 -0700

    Fix: frontend routing update for Vercel

--- frontend/src/app/leagues/[id]/layout.tsx
+++ frontend/src/app/leagues/[id]/layout.tsx
@@ -2,7 +2,7 @@
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import { cn } from '@/lib/utils';
+import { cn } from '../../../lib/utils';
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
 
 function classNames(...classes: string[]) {
